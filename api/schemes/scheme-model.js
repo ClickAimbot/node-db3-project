d3de5731040c43ef65aebb218dba0b6e6cf63dfe
Austin <austin.amaral3@gmail.com>
2023-10-28 18:56:42 -0700

    wip

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -117,7 +117,7 @@ async function findById(scheme_id) { // EXERCISE B
 
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -138,12 +138,28 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+      const rows = await db('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .select('st.step_id', 'st.step_number', 'instructions', 'sc.scheme_name')
+      .where('sc.scheme_id', scheme_id)
+      .orderBy('step_number')
+
+      if (!rows[0].step_id) {
+        return []
+      } else {  
+        return rows
+      }
 }
 
 function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+
+    return db('schemes').insert(scheme)
+      .then(([scheme_id]) => {
+        return db('schemes').where('scheme_id', scheme_id).first()
+      })
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
